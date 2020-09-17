48d11d1cab43ff27bb939b70cec40271ed208898
thormiwa <thormiwa04@gmail.com>
2020-09-17 09:49:29 +0000

    the c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the point
+ *
+ * Return: should be 0 (Success)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+} 
