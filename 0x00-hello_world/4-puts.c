12f479c6cc6f52e004edb7b379ad3f3d2856d879
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-17 12:13:23 +0100

    the c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *main - begins the code
+ *main - starts the code
  *Description: prints "Programming is like building a multilingual puzzle
  *Return: int
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
