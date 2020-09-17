66d2cdd2f9539cdff18c69754d202f5b2bb9e105
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-17 23:20:11 +0100

    make executable

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main - Prints all single digit numbers
+ * main - Print all single digit numbers
  *
  * Description: Without using the var char
- * Return: Always(0) Success
+ * Return: always(0) Success
  */
 int main(void)
 {
-  int num;
+	int num;
 
-  for (num = 0; num <= 9; num++)
-    putchar(num + '0');
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
 
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
