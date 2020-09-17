3e5987aa34a2105969285731fd69c47417d4d78b
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-17 23:10:59 +0100

    make executable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
- * main - entry point
- * Return: always 0 (success)
+ * main - finds the last interger in random number
+ *
+ * Description: The numbers are generated
+ * Return: Always(0) Success
  */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  printf("Last digit of ");
-  printf("%d", n);
-  printf(" is %d ", n % 10);
-  if ((n % 10) > 5)
-    {
-      printf("and is greater than 5\n");
-    }
-  else if ((n % 10) == 0)
-    {
-      printf("and is 0\n");
-    }
-  else
-    {
-      printf("and is less than 6 and not 0\n");
-    }
-  return (0);
- }
+	int n, lastInt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	lastInt = n % 10;
+
+	if (lastInt == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastInt);
+	else if (lastInt < 6 && lastInt != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastInt);
+	else if (lastInt > 5)
+		printf("Last Digit of %d is %d and is greater than 5\n", n, lastInt);
+
+	return (0);
+}
