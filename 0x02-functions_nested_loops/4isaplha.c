f8de16c62b56f23a05c5baeea20de3b0d3859f53
thormiwa <thormiwa04@gmail.com>
2020-09-22 12:25:02 +0000

    nested loopd

---
+++ 0x02-functions_nested_loops/4isaplha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *Returns 1 if c is a letter, lowercase or uppercase
+ *
+ *Return: Returns 0 otherwise
+ */
+
+int _isalpha(int c)
+
+{
+
+  if ((c > 97 && c < 122) || (c > 65 && c < 90))
+    {
+      return (1);
+    }
+
+  else
+    return (0);
+
+}
