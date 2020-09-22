6bcb3f18ec1d0ef68ef7a511a932898080c4d557
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-22 17:01:51 +0100

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,24 @@
 #include "holberton.h"
-
 /**
- * print_alphabet - prints all alphabets in lowercase
+ * print_alphabet_x10 - prints alphabets 10
  *
- * Description: They are sorted by default
- *
+ * Description: prints 10 times
+ * Return: Always(0) Success
  */
-
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-  char alphabet;
+	int x;
+	char c;
 
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-      _putchar(alphabet);
-    }
-  _putchar('\n');
+	x = 1;
+
+	while (x <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	x += 1;
+	}
 }
