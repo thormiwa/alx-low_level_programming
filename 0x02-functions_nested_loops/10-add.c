ffd88ba744a09f04d677de9a9b1cdca4d0b43bb6
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-22 21:55:28 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
-include "holberton.h"
+#include "holberton.h"
 /**
- *add-adds to numbers i and j
- *@i:number 1
- *@j:bumber 2
- *Return: returns i + j
- */
+*add-adds to numbers i and j
+*@i:number 1
+*@j:bumber 2
+*Return: returns i + j
+*/
 int add(int i, int j)
 {
-  return (i + j);
+	return (i + j);
 }
