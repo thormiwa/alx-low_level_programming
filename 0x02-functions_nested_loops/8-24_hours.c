77dc47e050cc6a7d795f6c8e4cd5363ad93d32ac
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-22 21:54:09 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,18 +4,18 @@
  */
 void jack_bauer(void)
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i < 24; i++)
-    {
-      for (j = 0; j < 60; j++)
+	for (i = 0; i < 24; i++)
 	{
-	  _putchar(i / 10 + 48);
-	  _putchar(i % 10 + 48);
-	  _putchar(':');
-	  _putchar(j / 10 + 48);
-	  _putchar(j % 10 + 48);
-	  _putchar('\n');
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
+		}
 	}
-    }
 }
