d67e98f584a42f27bb163ed6f40e114e9aeb1dd8
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-24 21:47:16 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 /**
- * print_diagonal - print a diagonal line of variable length
+ * print_diagonal - print a diagonal line
  * @n: length of line
  */
 void print_diagonal(int n)
 {
-  int i = 0, s;
+	int i = 0, s;
 
-  if (n > 0)
-    {
-      while (i < n)
+	if (n > 0)
 	{
-	  for (s = 0; s < i; s++)
-	    _putchar(' ');
+		while (i < n)
+		{
+			for (s = 0; s < i; s++)
+				_putchar(' ');
 
-	  _putchar('\\');
-	  _putchar('\n');
-	  i++;
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
 	}
-    }
-  else
-    _putchar('\n');
+	else
+		_putchar('\n');
 }
