6afbbf4d1f600659917a4ce30e97aaebde76fc09
thormiwa <thormiwa04@gmail.com>
2020-09-24 16:14:53 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints uppercase letters
+ * Return: Always(0) Success
+ */
+
+int _isdigit(int c)
+{
+  if (c >= 0 && c <= 9)
+    {
+      return (1);
+    }
+  else
+    return (0);
+}
