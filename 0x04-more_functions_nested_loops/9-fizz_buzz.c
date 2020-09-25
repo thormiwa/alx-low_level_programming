649ca45742c8f343aed6ff972c91ae3d0bc87688
thormiwa <thormiwa04@gmail.com>
2020-09-25 15:26:31 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints multplies of 3 or 5
+ *
+ * Description:
+ * Return: Always returns 0
+ */
+int main(void)
+{
+  int start, end;
+
+  end = 99;
+
+  for (start = 1; start <= end; start++)
+    {
+      if (start % 15 == 0)
+	{
+	  printf("FizzBuzz ");
+	}
+      else if (start % 3 == 0)
+	{
+	  printf("Fizz ");
+	}
+      else if (start % 5 == 0)
+	{
+	  printf("Buzz ");
+	} else
+	{
+	  printf("%d ", start);
+	}
+    }
+
+  printf("Buzz\n");
+
+  return (0);
+}
