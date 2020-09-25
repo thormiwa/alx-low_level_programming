c7e100c274bb1ae428c2003ff5ce498f23168f60
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-09-25 16:29:31 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 /**
- * main - prints multplies of 3 or 5
+ * main - prints nultplies of 3 or 5
  *
  * Description:
  * Return: Always returns 0
  */
 int main(void)
 {
-  int start, end;
+	int start, end;
 
-  end = 99;
+	end = 99;
 
-  for (start = 1; start <= end; start++)
-    {
-      if (start % 15 == 0)
+	for (start = 1; start <= end; start++)
 	{
-	  printf("FizzBuzz ");
+		if (start % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (start % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (start % 5 == 0)
+		{
+			printf("Buzz ");
+		} else
+		{
+			printf("%d ", start);
+		}
 	}
-      else if (start % 3 == 0)
-	{
-	  printf("Fizz ");
-	}
-      else if (start % 5 == 0)
-	{
-	  printf("Buzz ");
-	} else
-	{
-	  printf("%d ", start);
-	}
-    }
 
-  printf("Buzz\n");
+	printf("Buzz\n");
 
-  return (0);
+	return (0);
 }
