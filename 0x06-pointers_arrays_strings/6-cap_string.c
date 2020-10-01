a716d42b94e175708084d769d0003f12fee936d7
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-10-01 21:39:44 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,15 @@
 
 char *cap_string(char *s)
 {
-  int x;
-  x = 0;
+int x;
+x = 0;
 
-  while (s[x])
-    {
-      while (!(s[x] >= 'a' && s[x] <= 'z'))
-	x++;
+while (s[x])
+{
+while (!(s[x] >= 'a' && s[x] <= 'z'))
+x++;
 
-      if (s[x - 1] == ' ' ||
+if (s[x - 1] == ' ' ||
 s[x - 1] == '\t' ||
 s[x - 1] == '\n' ||
 s[x - 1] == ',' ||
@@ -31,12 +31,12 @@ s[x - 1] == '(' ||
 s[x - 1] == ')' ||
 s[x - 1] == '{' ||
 s[x - 1] == '}' ||
-	  x == 0)
-	s[x] -= 32;
+x == 0)
+s[x] -= 32;
 
-      x++;
-    }
+x++;
+}
 
-  return (s);
+return (s);
 
 }
