25dd106d340f1d1b7f8aeae063148bf1fdcff09a
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-10-01 21:37:48 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,12 @@
 
 void reverse_array(int *a, int n)
 {
-  int x, y;
+int x, y;
 
-  for (x = 0; x < (n / 2); x++)
-    {
-      y = a[x];
-      a[x] = a[n - x - 1];
-      a[n - x - 1] = y;
-    }
+for (x = 0; x < (n / 2); x++)
+{
+y = a[x];
+a[x] = a[n - x - 1];
+a[n - x - 1] = y;
+}
 }
