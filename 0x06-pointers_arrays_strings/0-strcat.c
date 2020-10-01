ea5bf1a4536b13c1113407a3e5c078e773614a52
thormiwa <thormiwa04@gmail.com>
2020-10-01 19:40:01 +0000

    pointers and arrays 2.0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,31 @@
-#include "holberton.h"
+include "holberton.h"
 
 /**
  *_strcat - A function that concatenates two strings
  *@dest: String to be concatenated to
  *@src: String to concatenate from
- *Return: dest
+ *Return: Returns concatenated string.
+ *
  */
 
 char *_strcat(char *dest, char *src)
 {
-int x, y;
+  int len;
+  int z;
 
-for (x = 0; dest[x] != '\0'; x++)
-{
-}
-for (y = 0; src[y] != '\0'; y++)
-{
-dest[x] = src[y];
-x++;
-}
-dest[x] = '\0';
-return (dest);
+  len = 0;
+  while (dest[len] != '\0')
+    {
+      ++len;
+    }
+
+  z = 0;
+  for (; src[z] != '\0'; z++, len++)
+    {
+      dest[len] = src[z];
+    }
+
+  dest[len] = '\0';
+
+  return (dest);
 }
