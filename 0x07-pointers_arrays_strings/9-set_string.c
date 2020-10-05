c2d76bb55df529f11e02c090041ca34aac384159
thormiwa <thormiwa04@gmail.com>
2020-10-05 21:02:21 +0000

    pointers 3.0

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char.
+ * @s: Pointer a pointer
+ * @to: Destination to be set to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
