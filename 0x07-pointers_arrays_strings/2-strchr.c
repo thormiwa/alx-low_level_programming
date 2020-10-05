cd3c531074c0ca8b977e40c8fb321a4cbdaa1c98
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-10-05 21:32:03 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -23,29 +23,4 @@ char *_strchr(char *s, char c)
 		return (s + x);
 	}
 	return (0);
-}#include "holberton.h"
-
-/**
- * _strchr - Locates a character in a string
- * @s: pointer.
- * @c: character.
- * Return: NULL or Pointer to the first occurence of char
- */
-
-char *_strchr(char *s, char c)
-{
-	int x;
-
-	for (x = 0; s[x] != '\0'; x++)
-	{
-		if (s[x] == c)
-		{
-			return (s + x);
-		}
-	}
-	if (s[x] == c)
-	{
-		return (s + x);
-	}
-	return (0);
 }
