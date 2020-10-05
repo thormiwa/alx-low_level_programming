60d207c503ee25f4bdd5c5e9b3c3d0ed0ddd0609
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-10-05 21:55:29 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -26,32 +26,4 @@ char *_strpbrk(char *s, char *accept)
 		}
 	}
 	return ('\0');
-}#include "holberton.h"
-
-/**
- * _strpbrk - Searches a string for any set of bytes.
- * @s: pointer to a string.
- * @accept: character.
- * Return: Returns a pointer that matches one of the bytes.
- */
-
-char *_strpbrk(char *s, char *accept)
-{
-	int a, b;
-
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		for (b = 0; accept[b] != '\0'; b++)
-		{
-			if (s[a] == accept[b])
-			{
-				return (s + a);
-			}
-		}
-		if (s[a] == accept[b])
-		{
-			return (s + a);
-
-
-
-
+}
