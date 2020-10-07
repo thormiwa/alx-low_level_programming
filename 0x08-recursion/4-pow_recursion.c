26d75243fee05a907a4b17807566aa5277265728
thormiwa <thormiwa04@gmail.com>
2020-10-07 19:19:55 +0000

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+
+/**
+ *_pow_recursion - raise x to the power y
+ *@x: base
+ *@y: power
+ *Return: x power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
