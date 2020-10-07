e7b657d6adc46d8ebf8527e4deb09beafc8e2055
thormiwa <thormiwa04@gmail.com>
2020-10-07 17:49:13 +0000

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_puts_recursion - pritns str recursivly
+ *@s: string
+ *Return: void
+ */
+
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(++s);
+	}
+}
