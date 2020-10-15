755c15389e404f3936024da0a61e9c5e5b8033ee
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-10-15 20:51:19 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,59 @@
+#include <stdlib.h>
 #include "holberton.h"
-
 /**
- * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: number of bytes from s2 to be copied
- * Return: concatenated string
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	unsigned int i, j, size, len1, len2;
-	char *ptr;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	if (n > len2)
-		n = len2;
-	size = len1 + n;
-	ptr = malloc(sizeof(char) * size + 1);
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		ptr[i] = s2[j];
-	ptr[i] = 0;
-	return (ptr);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
+ * _strlen - counts the length of string.
+ * @s: Value string check.
+ * Return: String.
  */
 int _strlen(char *s)
 {
-	char *p = s;
+	int i;
 
-	while (*s)
-		s++;
-	return (s - p);
+	for (i = 0; s[i] != '\0';)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * string_nconcat - concatenate two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: bytes for s2 to use.
+ * Return: Only both strings.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	unsigned int i, len1, len2;
+	char *c;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	c = malloc(sizeof(char) * (len1 + n + 1));
+	if (c == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		c[i] = s1[i];
+	for (; i < len1 + n; i++)
+		c[i] = s2[i - len1];
+	c[len1 + n] = '\0';
+
+	return (c);
 }
