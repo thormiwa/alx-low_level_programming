af15598bfc19f0c2fe143669fb0e9a47059d83d0
thormiwa <thormiwa04@gmail.com>
2020-10-16 08:51:31 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * _calloc - allocates memory for an array, using malloc
@@ -14,44 +15,31 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
+		free(ptr);
 		newMem = malloc(new_size);
-		if (newMem == NULL)
-		{
-			return (NULL);
-		}
 		return (newMem);
 	}
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else if (new_size == old_size)
-	{
+	if (new_size == old_size)
 		return (ptr);
-	}
 
 	newMem = malloc(new_size);
 	if (newMem == NULL)
-	{
+	
 		return (NULL);
-	}
+
 
 
 	for (i = 0; i < min; i++)
 	{
 		newMem[i] = ((char *)ptr)[i];
 	}
-	if (new_size > old_size)
-	{
-		while (i < (int)new_size)
-		{
-			newMem[i] = 0;
-			i++;
-		}
-	}
-
 
 	free(ptr);
+	
 	return (newMem);
 }
