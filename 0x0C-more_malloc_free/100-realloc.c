e8d6a044761a4a974f5a03a69bad1427896cbefa
thormiwa <70862950+thormiwa@users.noreply.github.com>
2020-10-16 09:26:37 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,93 +1,57 @@
 #include "holberton.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: input pointer
- * @old_size: size of old ptr
- * @new_size: size of new ptr
- * Return: reallocated ptr
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements of pointer
+ * @size: size of each member
+ * Return: pointer of allocated memory
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *res = NULL;
+	char *newMem;
+	int i, min = new_size < old_size ? new_size : old_size;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (!ptr)
+	if (ptr == NULL)
 	{
-		free(ptr);
-		res = malloc(new_size);
-		return (res);
+		newMem = malloc(new_size);
+		if (newMem == NULL)
+		{
+			return (NULL);
+		}
+		return (newMem);
 	}
-	if (!new_size && ptr)
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	res = malloc(new_size);
-	_memcpy(res, ptr, old_size);
-	free(ptr);
-	return (res);
-}
-
-/**
- * _memcpy - copies memory area
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to be copied
- * Return: pointer to dest
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	char *ptr = dest;
-
-	while (n--)
-		*dest++ = *src++;
-	return (ptr);
-}#include "holberton.h"
-
-/**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: input pointer
- * @old_size: size of old ptr
- * @new_size: size of new ptr
- * Return: reallocated ptr
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *res = NULL;
-
-	if (new_size == old_size)
+	else if (new_size == old_size)
+	{
 		return (ptr);
-	if (!ptr)
-	{
-		free(ptr);
-		res = malloc(new_size);
-		return (res);
 	}
-	if (!new_size && ptr)
+
+	newMem = malloc(new_size);
+	if (newMem == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
-	res = malloc(new_size);
-	_memcpy(res, ptr, old_size);
+
+
+	for (i = 0; i < min; i++)
+	{
+		newMem[i] = ((char *)ptr)[i];
+	}
+	if (new_size > old_size)
+	{
+		while (i < (int)new_size)
+		{
+			newMem[i] = 0;
+			i++;
+		}
+	}
+
+
 	free(ptr);
-	return (res);
-}
-
-/**
- * _memcpy - copies memory area
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to be copied
- * Return: pointer to dest
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	char *ptr = dest;
-
-	while (n--)
-		*dest++ = *src++;
-	return (ptr);
+	return (newMem);
 }
